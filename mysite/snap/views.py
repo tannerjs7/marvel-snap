8411b1a182ad13001870abb85dc06df2cb626e06
Tanner Schilperoort <tannerschilperoort@gmail.com>
2024-11-07 16:52:13 -0500

    Change nav styles, combine starter/recruit stats
    Also changed pool column to display full names.

--- mysite/snap/views.py
+++ mysite/snap/views.py
@@ -16,33 +16,30 @@ def index(request):
 def stats(request):
     cards = Card.objects.all()
     owned_rates = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
-    pools = ['Starter', 'Recruit', 'Pool 1', 'Pool 2', 'Pool 3', 'Pool 4', 'Pool 5', 'All']
+    pools = ['Starter/Recruit', 'Pool 1', 'Pool 2', 'Pool 3', 'Pool 4', 'Pool 5', 'All']
 
     for card in cards:
-        if card.pool == 'starter':
+        if card.pool == 'starter' or card.pool =='recruit':
             owned_rates[0][1] += 1
             if card.owned == True: owned_rates[0][0] += 1
-        elif card.pool == 'recruit':
+        elif card.pool == '1':
             owned_rates[1][1] += 1
             if card.owned == True: owned_rates[1][0] += 1
-        elif card.pool == '1':
+        elif card.pool == '2':
             owned_rates[2][1] += 1
             if card.owned == True: owned_rates[2][0] += 1
-        elif card.pool == '2':
+        elif card.pool == '3':
             owned_rates[3][1] += 1
             if card.owned == True: owned_rates[3][0] += 1
-        elif card.pool == '3':
+        elif card.pool == '4':
             owned_rates[4][1] += 1
             if card.owned == True: owned_rates[4][0] += 1
-        elif card.pool == '4':
+        elif card.pool == '5':
             owned_rates[5][1] += 1
             if card.owned == True: owned_rates[5][0] += 1
-        elif card.pool == '5':
+        if card.pool != 'none':
             owned_rates[6][1] += 1
             if card.owned == True: owned_rates[6][0] += 1
-        if card.pool != 'none':
-            owned_rates[7][1] += 1
-            if card.owned == True: owned_rates[7][0] += 1
 
     context = {'cards': cards,
                'owned_rates': owned_rates,
