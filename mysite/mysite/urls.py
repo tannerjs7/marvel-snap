177e667d091665b4d26833e961d590ae7ff74451
Tanner Schilperoort <tannerschilperoort@gmail.com>
2024-12-15 21:09:07 -0500

    Add background gradient, redirect base URL

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     path('snap/', include('snap.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='snap/cards', permanent=False))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
